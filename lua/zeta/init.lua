f06915132e7df858b2cb84512ad83023e6032d98
Seongmin Lee <boltlessengineer@gmail.com>
2025-02-16 08:57:57 +0000

    fix: automatically setup highlights without `setup()` call

--- lua/zeta/init.lua
+++ lua/zeta/init.lua
@@ -1,14 +1,8 @@
 local M = {}
 
-local function hl_setup()
-    vim.api.nvim_set_hl(0, "ZetaDiffAdd", { link = "DiffAdd" })
-    vim.api.nvim_set_hl(0, "ZetaDiffDelete", { link = "DiffDelete" })
-end
-
 ---@param opts? zeta.Opts
 function M.setup(opts)
     vim.g.zeta_nvim = opts
-    hl_setup()
 end
 
 return M
--- plugin/zeta.lua
+++ plugin/zeta.lua
@@ -1,1 +1,4 @@
 -- require("zeta.autocmd").setup()
+
+vim.api.nvim_set_hl(0, "ZetaDiffAdd", { link = "DiffAdd" })
+vim.api.nvim_set_hl(0, "ZetaDiffDelete", { link = "DiffDelete" })
